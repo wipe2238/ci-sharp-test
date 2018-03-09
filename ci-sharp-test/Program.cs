38bbb2181321b23c5cd261780f0d2bc278a77d62
Wipe <wipe.git@gmail.com>
2018-03-09 11:38:01 +0100

    nuget

--- ci-sharp-test/Program.cs
+++ ci-sharp-test/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace ci_sharp_test
 {
@@ -11,6 +12,9 @@ namespace ci_sharp_test
 		[STAThread]
 		static void Main()
 		{
+			JsonTextReader reader = null;
+			reader = default( JsonTextReader );
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault( false );
 			Application.Run( new Forms.Main() );
